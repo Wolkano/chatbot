d5c037e0949fa9dd6d33687def99de9a78240db8
Magnusb99 <magnusb99@hotmail.se>
2025-02-24 14:43:06 +0100

    Chatbot

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="ChatContainer">
-    <div class="firstq"><h1>Hej Bygg-Bengt här!</h1>
+    <div class="firstq">
+      <h1>Hej Bygg-Bengt här!</h1>
       <p>Vill du få en offert inom 5 minuter?</p>
       <button @click="startForm()" class="yes">Ja</button>
       <button class="no">Nej</button>
@@ -8,36 +9,29 @@
     <form>
       <div class="question"><p class="qText"></p></div>
       <div class="answer"></div>
-
     </form>
   </div>
 </template>
 
 <script setup>
-//import Coockies from 'js-cookie';
+import Cookies from "js-cookie";
 let currentQ = 0;
 
-
-
 const startForm = () => {
-  document.querySelector(".firstq").style="display: none";
+  document.querySelector(".firstq").style = "display: none";
 
-document.querySelector(".qText").textContent = "Vilken offert gäller det?";
+  document.querySelector(".qText").textContent = "Vilken offert gäller det?";
 
-const svarsDiv = document.querySelector(".answer");
+  const svarsDiv = document.querySelector(".answer");
 
-const options = ["Dränering","Fasad","Golv"];
+  const options = ["Dränering", "Fasad", "Golv"];
 
-options.forEach(option => {
-  const btn = document.createElement("button");
- btn.innerText = option;
- btn.onclick = () => handleResponse(option);
-svarsDiv.appendChild(btn);
-
-});
-
-
-
+  options.forEach((option) => {
+    const btn = document.createElement("button");
+    btn.innerText = option;
+    btn.onclick = () => handleResponse(option);
+    svarsDiv.appendChild(btn);
+  });
 };
 
 const addQuestion = (q) => {
@@ -47,105 +41,144 @@ const addQuestion = (q) => {
 
   const input = document.createElement(q.element);
 
-  console.log(q.text + q.element + q.type + q.min + q.max + q.step + q.options + q.id)
-  
-  if(q.type === "slider"){
+  if (q.type === "slider") {
     input.type = "range";
-  input.min = q.min;
-  input.max = q.max;
-  input.step = q.tep;
-  input.value = (q.min+q.max)/2;
-  
-  const valueDisplay = document.createElement("p");
-        valueDisplay.innerText = `Värde: ${input.value} meter`;
+    input.name = q.id;
+    input.min = q.min;
+    input.max = q.max;
+    input.step = q.step;
+    input.value = (q.min + q.max) / 2;
 
-        input.oninput = () => {
-            valueDisplay.innerText = `Värde: ${input.value} meter`;
-        };
-        aDiv.appendChild(input);
-        aDiv.appendChild(valueDisplay);
+    const valueDisplay = document.createElement("p");
+    valueDisplay.innerText = `Värde: ${input.value} meter`;
 
-
-  } else if (q.type === "select"){
-
-    q.options.forEach(option => {
-      const optionEl = document.createElement("option");
+    input.oninput = () => {
+      valueDisplay.innerText = `Värde: ${input.value} meter`;
+    };
+    aDiv.appendChild(input);
+    aDiv.appendChild(valueDisplay);
+  } else if (q.type === "radio") {
+    q.options.forEach((option) => {
+      const optionEl = document.createElement("input");
+      const optionLab = document.createElement("label");
+      optionLab.textContent = option;
+      optionEl.type = q.type;
       optionEl.value = option;
-      optionEl.innerText = option;
 
-      input.appendChild(optionEl);
-      
+      optionEl.name = q.id;
+      aDiv.appendChild(optionEl);
+      aDiv.appendChild(optionLab);
     });
- 
   }
-
-
-
-
 };
 
 const draneringsOffert = () => {
-  
   const questions = [
-    { text: "Ange längden på respektive vägg alternativt hela sträckan som schaktats upp. Över 70 löpmeter?", 
-    element: "input",
-    type: "slider", 
-    min: 5, 
-    max: 100, 
-    step: 5, 
-    options: null,
-    id: 1,
-  },
-    {text: "Tjocklek isoleringsskiva",
-    element: "select",
-      type: "select",
+    {
+      text: "Ange längden på respektive vägg alternativt hela sträckan som schaktats upp. Över 70 löpmeter?",
+      element: "input",
+      type: "slider",
+      min: 1,
+      max: 70,
+      step: 1,
+      options: null,
+      id: 1,
+    },
+    {
+      text: "Väggdjup är måttet från marknivå till golvet i källare. För att räkna ut det gör enligt följande 1. Mät från innergolv upp till ett källarfönsters underkant. (X) 2. Mät på utsidan från fönstrets underkant ner till marknivå. (Y) 3. Beräkna enligt följande: Väggdjup = X - Y",
+      element: "input",
+      type: "slider",
+      min: 0.1,
+      max: 4.0,
+      step: 0.1,
+      options: null,
+      id: 2,
+    },
+    {
+      text: "100 mm tjocklek ger mycket bra fuktsäkerhet och värmeisolering. 200 mm uppfyller kraven enligt Boverkets regler ur energisynpunkt vid nyproduktion eller byggnadslovspliktig utbyggnad. Med 200 mm får du alltid dubbel dräneringskapacitet, ännu större uttorkning av väggen och lägre energiförbrukning jämfört med 100 mm.",
+      element: "input",
+      type: "radio",
       min: null,
       max: null,
       step: null,
-      options:["100mm","200mm"],
-      id: 2,
-    }
-
-
+      options: ["100mm", "200mm"],
+      id: 3,
+    },
+    {
+      text: "Vilken typ av dräneringsbrunn som ska användas avgörs av vilka möjligheter som finns att på ett säkert sätt avleda dräneringsvattnet från fastigheten. Vi har tagit fram lösningar för de vanligaste förekommande fallen.",
+      element: "input",
+      type: "radio",
+      min: null,
+      max: null,
+      step: null,
+      options: [
+        "Pumpbrunn (vanligast)",
+        "Dräneringsbrunn utan pump",
+        "Egen lösning",
+      ],
+      id: 4,
+    },
+    {
+      text: "Välj om överblivna schaktmassor ska förslas bort eller om du behandlar massorna på egen hand. Tänk på att det blir ca 40 - 50 kubikmeter schaktmassor kvar efter återfyllningen.",
+      element: "input",
+      type: "radio",
+      min: null,
+      max: null,
+      step: null,
+      options: ["Entreprenören forslar bort", "Egen Bortforsling"],
+      id: 5,
+    },
   ];
   questionHandler(questions);
-
-
-
 };
 const questionHandler = (questions) => {
+  let svar;
+  if (currentQ >= questions.length) {
+    const aDiv = document.querySelector(".answer");
+    const svarsLista = document.createElement("ul");
+    questions.forEach((question) => {
+      const svar = document.createElement("li");
 
+      const value = JSON.parse(Cookies.get(question.id));
 
+      svar.textContent = value;
+      svarsLista.appendChild(svar);
+    });
+    aDiv.appendChild(svarsLista);
+  } else {
+    const question = questions[currentQ];
+    addQuestion(question);
 
-  const question = questions[currentQ];
+    const continueBtn = document.createElement("button");
+    continueBtn.innerText = "Bekräfta";
+    continueBtn.onclick = () => {
+      if (question.type === "slider") {
+        svar = document.querySelector(`input[name= "${question.id}"]`);
+      } else if (question.type === "radio") {
+        svar = document.querySelector(`input[name= "${question.id}"]:checked`);
+      }
+      Cookies.set(question.id, JSON.stringify(svar.value), {
+        expires: 1,
+        path: "",
+      });
 
-  addQuestion(question);
+      currentQ++;
 
-  const continueBtn = document.createElement("button");
-continueBtn.innerText = "Bekräfta";
-continueBtn.onclick = () => {
-  currentQ++
-  //Coockies.set(q.id,input.value,{expires: 1, path: ''});
-  questionHandler(questions);
-  
-}
-document.querySelector(".answer").appendChild(continueBtn);
+      document.querySelector(".answer").innerHTML = "";
+      document.querySelector(".qText").innerHTML = "";
 
-
-if(currentQ>=questions.length){
-  currentQ = 0;
-}
-
-
-  
-}
-
+      questionHandler(questions);
+    };
+    const aDiv = document.querySelector(".answer");
+    aDiv.appendChild(continueBtn);
+  }
+};
 
 const handleResponse = (op) => {
   document.querySelector(".qText").innerHTML = "";
   document.querySelector(".answer").innerHTML = "";
-  
-  switch(op){
+
+  switch (op) {
     case "Dränering":
       draneringsOffert();
       break;
@@ -159,16 +192,10 @@ const handleResponse = (op) => {
       console.log("Något gick fel");
   }
 };
-
-
-
-
-
 </script>
 
 <style lang="scss">
-
-.ChatContainer{
+.ChatContainer {
   border: 2px solid black;
   border-radius: 5px;
   width: 50vw;
